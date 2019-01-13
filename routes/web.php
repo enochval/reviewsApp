ac09f3587977671185f0058b6ace3cd37cf734f6
enochval <e.osarenren@dreammesh.ng>
2019-01-13 17:47:59 +0100

    built js

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/a', 'ReviewsCommentsController@amazon');
-
 Route::post('reviews', 'ReviewsCommentsController@review');
