a570c617e6aa37933d3794189c817c12197fbfc5
enochval <e.osarenren@dreammesh.ng>
2019-01-13 17:42:44 +0100

    fixed composer ish

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Alaouy\Youtube\YoutubeServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -224,7 +223,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Youtube' => Alaouy\Youtube\Facades\Youtube::class,
 
     ],
 
